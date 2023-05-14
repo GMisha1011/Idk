01575be6a2b910f4f463bce88fc4eaab6f8c705d
GMisha1011 <gmisha1011@gmail.com>
2023-05-14 13:44:14 +0300

    End of lesson3

--- lesson3/main.py
+++ lesson3/main.py
@@ -4,3 +4,18 @@ Kirril = Human("Kirril", 1)
 Nikita = Human("Nikita", 0)
 Mykhail = Human("Mykhail", 0)
 Pavlo = Human("Pavlo", 0)
+humans = list()
+humans.append(Kirril)
+humans.append(Nikita)
+humans.append(Mykhail)
+humans.append(Pavlo)
+auto = Auto("BMW X7")
+for Human in humans:
+    if(Human.Role == 1):
+        auto.AddDrivers(Human)
+    else:
+        auto.AddPassengers(Human)
+for driver in auto.Drivers:
+    print(f"Driver of {auto.Brand} is {driver}")
+for Passenger in auto.Passengers:
+    print(f"Passengers: {Passengers}")
