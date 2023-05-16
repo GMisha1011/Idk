327e9e307fd26ded1019314288d62cdf0d9479f6
GMisha1011 <gmisha1011@gmail.com>
2023-05-16 21:08:30 +0300

    DZ 2

---
+++ DZ 2/cat.py
@@ -0,0 +1,8 @@
+class Cat:
+#constructor __init__
+    def __init__(self, color, name, age ,healthcondition, uniquethings):
+        self.Name = name
+        self.Color = color
+        self.Age = age
+        self.HealthCondition = healthcondition
+        self.UniqueThings = uniquethings
---
+++ DZ 2/main.py
@@ -0,0 +1,4 @@
+from cat import Cat
+
+Archik = Cat("Brown", "Archik", 13 ,"Good", "Got some cool drip")
+print(Archik.__str__())
