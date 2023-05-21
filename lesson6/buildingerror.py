0ab6563a1aefcba64be2013a9ffcf8224ea84aad
GMisha1011 <gmisha1011@gmail.com>
2023-05-21 15:24:57 +0300

    lesson 6

---
+++ lesson6/buildingerror.py
@@ -0,0 +1,7 @@
+class buildingerror(Exception):
+    def __init__(self, amount, limit):
+        self.Amount = amount
+        self.Limit = limit
+    def __str__(self):
+        return f"Amount: {self.Amount} is greater then limit: {self.Limit}"
+
---
+++ lesson6/main.py
@@ -0,0 +1,40 @@
+import warnings
+"""
+'''
+try:
+your_code #requests, database, arythmetic proccess...
+except ...:
+proc_error/exception
+'''
+while(True):
+    try:
+        digit1 = 10
+        #warnings.warn("\nPossible zero division!\n")
+        digit2 = int(input("Enter digit: "))
+        res = digit1/digit2
+        print(res)
+    except ZeroDivisionError as zde:
+            print(f"Custom proccess: {zde}")
+    except Exception as ex:
+            print(ex)
+    yes = input("Enter Y for continue or N for stop: ")
+    if(yes.lower() == "n"):
+        break
+print("Cycle while has stoped!")
+"""
+
+from buildingerror import BuildingError
+from validation import Validation
+limit = 10
+amount = int(input("Enter amount: "))
+check = Validation(amount, limit)
+error = BuildingError()
+try:
+    check.Check()
+except ZeroDivisionError as zde:
+    print(f"Zero Division Error: {zde}")
+except BuildingError as be:
+    print(f"Custom Error Message\n"
+          f"{be}")
+except Exception as ex:
+    print(ex)
